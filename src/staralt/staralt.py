6959ce0fe67b362a27fd496d7d78609b9f97c96c
Elli Nestlerode <enestl@uw.edu>
2025-04-10 20:28:08 +0000

    Completed the get_altitude() function

--- src/staralt/staralt.py
+++ src/staralt/staralt.py
@@ -3,9 +3,14 @@
 #
 # @Filename: staralt.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+import numpy as np
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation
+from astropy.coordinates import ICRS, Galactic, FK4, FK5
+import astropy.units as u
 
 
-def get_altitude(coords, date):
+def get_altitude(coords_string, date_string):
     """Returns the altitude of the object at the given date.
 
     Parameters
@@ -21,8 +26,21 @@ def get_altitude(coords, date):
         The altitude of the object in degrees, as a float.
 
     """
+    # Takes inputs for form: 
+    # (["1:33:50.89 +30:39:36.633", "5:14:32.3 -8:12:5.1"], "2025-4-10 23:00:00")
+    
+    #Transforming coords_string and date_string into SkyCoords and astropy Time variables
+    coords = SkyCoord(coords_string, unit=(u.hourangle, u.deg), frame='icrs')
+    date = Time(date_string)
 
-    raise NotImplementedError("This function is not implemented yet.")
+    # Get altitude and azimuth
+    obs_location = EarthLocation(lat=41.3*u.deg, lon=-74*u.deg, height=0*u.m)
+    object_altaz = coords.transform_to(AltAz(obstime=date, location=obs_location))
+
+    # Return value for altitude
+    print(f"Object's Altitude = {object_altaz.alt}")
+    return object_altaz
+
 
 
 def plot_altitude():
