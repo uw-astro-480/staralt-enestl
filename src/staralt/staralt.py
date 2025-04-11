00660ec6c138b7ba5b7f2668389548a97f6649cd
Elli Nestlerode <enestl@uw.edu>
2025-04-11 16:08:07 +0000

    plot_altitude() function

--- src/staralt/staralt.py
+++ src/staralt/staralt.py
@@ -4,8 +4,9 @@
 # @Filename: staralt.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 import numpy as np
+import matplotlib.pyplot as plt
 from astropy.time import Time
-from astropy.coordinates import SkyCoord, AltAz, EarthLocation
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation, get_body
 from astropy.coordinates import ICRS, Galactic, FK4, FK5
 import astropy.units as u
 
@@ -34,7 +35,7 @@ def get_altitude(coords_string, date_string):
     date = Time(date_string)
 
     # Get altitude and azimuth
-    obs_location = EarthLocation(lat=41.3*u.deg, lon=-74*u.deg, height=0*u.m)
+    obs_location = EarthLocation.of_site('Apache Point Observatory')
     object_altaz = coords.transform_to(AltAz(obstime=date, location=obs_location))
 
     # Convert to float
@@ -49,7 +50,35 @@ def get_altitude(coords_string, date_string):
 
 
 
-def plot_altitude():
-    """Plots the altitude of the object over time, for a certain night."""
+def plot_altitude(coords_string, date_string):
+    date = date_string.split()[0]
+    time = Time(f"{date} 20:00:00")
+    time_intervals = np.linspace(0, 12, 80) * u.hour  # 80 time steps over 12 hours
+    all_times = time + time_intervals
+    observer_location = EarthLocation.of_site('Apache Point Observatory')
 
-    raise NotImplementedError("This function is not implemented yet.")
+    altaz_frame = AltAz(obstime=all_times, location=observer_location)
+
+    plt.figure(figsize=(10, 6))
+
+    for coord_str in coords_string:
+        obj = SkyCoord(coord_str, unit=(u.hourangle, u.deg), frame='icrs')
+        obj_altaz = obj.transform_to(altaz_frame)
+        plt.plot(all_times.datetime, obj_altaz.alt.deg, label=f"Object: {coord_str}")
+
+    moon = get_body('moon', time=all_times, location=observer_location)
+    moon_altaz = moon.transform_to(altaz_frame)
+    plt.plot(all_times.datetime, moon_altaz.alt.deg, label='Moon', color='red')
+
+    # Final plot formatting
+    plt.axhline(0, color='gray', linestyle='--', label='Horizon')
+    plt.title("Altitude Plot (Object and Moon)")
+    plt.xlabel("Time")
+    plt.ylabel("Altitude (degrees)")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.xticks(rotation=45)
+    plt.savefig('altitude_plot.png')
+    plt.show()
+
